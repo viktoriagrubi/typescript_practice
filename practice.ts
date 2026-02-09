07ff126616736d1b14e9f9bdb2b0a5d5bff0317b
viktoriagrubi <wiktoria.grubizna@op.pl>
2026-02-09 13:47:05 +0100

    add variable declaration

---
+++ practice.ts
@@ -0,0 +1,7 @@
+// deklaracja zmiennych:
+
+let name: string = "Wiktoria";
+let age: number = 25;
+let isActive: boolean = true;
+let nullValue: null = null;
+let undefinedValue: undefined = undefined;
